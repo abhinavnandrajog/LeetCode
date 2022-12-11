95bd932a213235948e7bf7f758d6c7c3f4668023
Abhinav Nandrajog <abhinavnandrajog14@gmail.com>
2022-12-11 23:07:09 +0530

    Create 1704. Determine if String Halves Are Alike.java

---
+++ 1704. Determine if String Halves Are Alike.java
@@ -0,0 +1,24 @@
+/*  https://leetcode.com/problems/determine-if-string-halves-are-alike/description/ */
+class Solution {
+    public boolean halvesAreAlike(String s) {
+        int n = s.length()/2;
+        String fin = s.toLowerCase();
+        String a = fin.substring(0,n);
+        String b = fin.substring(n,s.length());
+        int count1 = 0;
+        int count2 = 0;
+        for(int i = 0;i<n;i++){
+            if(a.charAt(i) == 'a' || a.charAt(i) == 'e' ||a.charAt(i) == 'i'|| a.charAt (i) == 'o'||a.charAt(i) == 'u'){
+                count1++;
+            }
+            if(b.charAt(i) == 'a'||b.charAt(i) =='e'||b.charAt(i) =='i'||b.charAt(i) =='o'||b.charAt(i) =='u'){
+                count2++;
+            }
+        }
+        if(count1 == count2){
+            return true;
+        }
+        return false;
+
+    }
+}
